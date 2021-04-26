df66d2628381c6487585909abc63e991a4722b48
Omar Reyna <omar.jesus.011@gmail.com>
2021-04-25 19:27:20 -0500

    Agregue una tabla adicional

--- database_dam_1/db_dam_1.sql
+++ database_dam_1/db_dam_1.sql
@@ -5,14 +5,20 @@ drop table if exists `tb_alumnos`;
 create table `tb_alumnos`(
 id int (8) not null primary key,
 nombre varchar(50) not null,
-estado char not null 
+estado char not null
 );
 
 drop table if exists `tb_profesores`;
 create table `tb_profesores`(
 id int (8) not null primary key,
 nombre varchar(50) not null,
-estado char not null 
+estado char not null
 );
 
 
+drop table if exists `tb_administrador`;
+create table `tb_administrador`(
+id int (8) not null primary key,
+nombre varchar(50) not null,
+estado char not null
+);
