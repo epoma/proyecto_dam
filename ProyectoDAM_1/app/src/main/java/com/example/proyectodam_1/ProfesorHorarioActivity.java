b6227e4399fa7043906824e2a0ac611673565310
Leonardo <55609886+celeonz@users.noreply.github.com>
2021-05-19 17:07:29 -0500

    Add files via upload

---
+++ ProyectoDAM_1/app/src/main/java/com/example/proyectodam_1/ProfesorHorarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectodam_1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfesorHorarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profesor_horario);
+    }
+}
---
+++ ProyectoDAM_1/app/src/main/java/com/example/proyectodam_1/ProfesorLogeoActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectodam_1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfesorLogeoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profesor_logeo);
+    }
+}
